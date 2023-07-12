90eb507068b2fe23882f0ef70230d22223681bd8
stasisqwq <1471452430@qq.com>
2023-07-12 16:12:32 +0800

    CommitFromKevin

--- mdclub/src/Facade/Model/TokenModel.php
+++ mdclub/src/Facade/Model/TokenModel.php
@@ -1,20 +1,20 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types=1);//废话
 
-namespace MDClub\Facade\Model;
+namespace MDClub\Facade\Model; // 与C++一致
 
-use MDClub\Model\Token;
+use MDClub\Model\Token; // Like using std::cin;
 
 /**
  * TokenModel Facade
  */
-class TokenModel extends Abstracts
+class TokenModel extends Abstracts // like C++ class
 {
     /**
      * @inheritDoc
      */
-    protected static function getFacadeAccessor(): string
+    protected static function getFacadeAccessor(): string // any question?
     {
         return Token::class;
     }
